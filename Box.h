1bed14273bbd4e8fb9d3f1eb74629336fce661a3
IT21044304 <87602649+ChamathAnuradha@users.noreply.github.com>
2021-08-18 13:01:33 +0000

    Exercise 2: Changed Box.cpp file. Changed Box.h Header File. Changed Exercise02.cpp file.

--- Box.h
+++ Box.h
@@ -6,6 +6,13 @@ class Box {
     public:
        // write prototypes of setters for length, width and height
        // write prototypes of getters for length, width and height
+      void setLength(int plength);
+      void setWidth(int pwidth);
+      void setHeight(int pheight);
+
+      int getLength();
+      int getWidth();
+      int getHeight();
  
-       int calcVolume();
+      int calcVolume();
 };
